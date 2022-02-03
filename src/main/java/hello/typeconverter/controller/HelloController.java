6ece9b30d28a0be604b3d72415d5e32039807a9c
woo00oo <96woo94@naver.com>
2022-02-03 19:17:45 +0900

    스프링 타입 컨버터 소개

---
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,25 @@
+package hello.typeconverter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data"); //문자 타입 조회
+        Integer intValue = Integer.valueOf(data);
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) { //스프링이 중간에서 타입을 변환해줌.
+        System.out.println("data = " + data);
+        return "ok";
+    }
+}
